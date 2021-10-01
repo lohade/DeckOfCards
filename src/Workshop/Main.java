03b45aa1c192660ee90e4905aa81cffcb21e95df
lohade <abhisheklohade8584@gmail.com>
2021-10-01 08:25:00 +0530

    welcome to Deck of cards game

---
+++ src/Workshop/Main.java
@@ -0,0 +1,9 @@
+package Workshop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Deck of cards:");
+    }
+}
