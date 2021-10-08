eb7fdc25f87f387032aaa674d12439afada57c1f
lohade <abhisheklohade8584@gmail.com>
2021-10-08 17:42:20 +0530

    adding player with condition

---
+++ src/Workshop/CardsFunction.java
@@ -0,0 +1,37 @@
+package Workshop;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CardsFunction {
+    Scanner scanner=new Scanner(System.in);
+
+    ArrayList<String> cards=new ArrayList<>();
+    //declare suit
+    String[] suit=new String[]{"Clubs","Diamonds","Hearts","Spades"};
+    //Declare ranks array
+    String[] ranks=new String[]{"2","3","4","5","6","7","8","9","10","Jacks","queen","King","Ace"};
+
+    public void createCards(){
+        for(int i=0;i< suit.length;i++) {
+            for (int j = 0; j < ranks.length; j++) {
+                cards.add(suit[i]+"->"+ranks[j]);
+            }
+        }
+        display(cards);
+    }
+
+    public void display(ArrayList<String> cards){
+        for(String card:cards) {
+            System.out.println(card);
+        }
+    }
+
+    public void addPlayer(){
+        System.out.println("how many player to add:");
+        int noOfPlayer= scanner.nextInt();
+        if(noOfPlayer>2 || noOfPlayer<5){
+            System.out.println("enter no of player between 2 to 4");
+        }
+    }
+}
--- src/Workshop/DeckOfCards.java
+++ src/Workshop/DeckOfCards.java
@@ -1,42 +1,12 @@
 package Workshop;
 
 public class DeckOfCards{
-    int[] Array=new int[52];
-   //Declare ranks array
-    String[] ranks=new String[]{"2","3","4","5","6","7","8","9","10","Jacks","queen","King","Ace"};
-    //declare suit
-    String[] suit=new String[]{"Clubs","Diamonds","Hearts","Spades"};
 
-    public String[] getRanks() {
-        return ranks;
-    }
-
-    public void setRanks(String[] ranks) {
-        this.ranks = ranks;
-    }
-    //defegit
-    public String[] getSuit() {
-        return suit;
-    }
-
-    public void setSuit(String[] suit) {
-        this.suit = suit;
-    }
-    //distributed cards
-    public void distributed(){
-        for(int i=0;i< ranks.length;i++) {
-            for (int j = 0; j < suit.length; j++) {
-                System.out.println(ranks[i]+"->"+suit[j]);
-            }
-        }
-    }
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Welcome to Deck of cards:");
-        DeckOfCards deck=new DeckOfCards();
-        deck.distributed();
-
-
-
+        CardsFunction cardsFunction=new CardsFunction();
+        System.out.println("Welcome to Deck of cards");
+        cardsFunction.createCards();
+        cardsFunction.addPlayer();
     }
 }
